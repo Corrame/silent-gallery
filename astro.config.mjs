bb0e3edf766569348ed84dd3095c306c82a5e221
corrame <31909207+Corrame@users.noreply.github.com>
2025-07-30 19:29:18 +0800

    redeploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  adapter: vercel({
-    // 关键新增：明确地将我们的API路由，排除在Astro的预渲染和中间件处理之外
-    functionPerRoute: false,
-    imageService: true,
-  }),
+  adapter: vercel(),
 });
